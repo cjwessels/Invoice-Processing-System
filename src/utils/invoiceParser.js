f815e84238392afc6cbc529918a71f9e114bcf68
Christiaan Wessels <97080537+cjwessels@users.noreply.github.com>
2025-05-13 16:32:01 +0200

    Updated invoiceParser.js

--- src/utils/invoiceParser.js
+++ src/utils/invoiceParser.js
@@ -74,7 +74,7 @@ const extractSupplierName = (text, fileName) => {
       regex: /Theewaterskloof Municipality/i,
       name: 'Theewaterskloof Municipality',
     },
-    { regex: /Wispernet Melkhoutfontein/i, name: 'Wispernet Melkhoutfontein' },
+    { regex: /WISPERNET/i, name: 'Wispernet Melkhoutfontein' },
     // Add other patterns as needed
   ];
 
