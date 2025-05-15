b2a48a9b22b1e7a7b2b7372c0e664ab0bf5c0268
Christiaan Wessels <97080537+cjwessels@users.noreply.github.com>
2025-05-15 14:05:54 +0200

    Updated invoiceParser.js

--- src/utils/invoiceParser.js
+++ src/utils/invoiceParser.js
@@ -163,7 +163,7 @@ const extractInvoiceDate = (text) => {
   const trustInvoiceDates = text.match(/Tax Invoice\s*(\d{1,2}\/\d{1,2}\/\d{2})/i);
   console.log(trustInvoiceDates)
   // const trustMatch = text.match(trustDatePattern);
-  if (trustInvoiceDates && trustInvoiceDates > 0) {
+  if (trustInvoiceDates && trustInvoiceDates.length > 0) {
     return trustInvoiceDates[1];
   }
 
