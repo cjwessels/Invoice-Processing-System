896bbef3b520714a096bd607967ac173823275ec
Christiaan Wessels <97080537+cjwessels@users.noreply.github.com>
2025-05-15 10:32:30 +0200

    Updated invoiceParser.js

--- src/utils/invoiceParser.js
+++ src/utils/invoiceParser.js
@@ -170,7 +170,7 @@ const extractInvoiceDate = (text) => {
 
   for (const pattern of datePatterns) {
     const match = text.match(pattern);
-    console.log(text)
+    console.log(match)
     const formatedDate = new Date(text)
     if (match && match[1]) {
       return formatedDate
