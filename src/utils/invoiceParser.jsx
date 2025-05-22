346cb39f13876df268ef6165f6b671cf5f7b8046
Christiaan Wessels <97080537+cjwessels@users.noreply.github.com>
2025-05-22 15:46:27 +0200

    Updated invoiceParser.jsx

--- src/utils/invoiceParser.jsx
+++ src/utils/invoiceParser.jsx
@@ -164,7 +164,8 @@ export const extractInvoiceDate = (text) => {
   }
 
   const datePatterns = [
-    /Invoice Date:|Invoice Date?\s*(\d{1,2}[\/.-]\d{1,2}[\/.-]\d{2,4})/i,
+    /Invoice Date:?\s*(\d{1,2}[\/.-]\d{1,2}[\/.-]\d{2,4})/i,
+    /Invoice Date?\s*(\d{1,2}[\/.-]\d{1,2}[\/.-]\d{2,4})/i,
     /Date:?\s*(\d{1,2}[\/.-]\d{1,2}[\/.-]\d{2,4})/i,
     /DATE OF ACCOUNT:?\s*(\d{1,2}[\/.-]\d{1,2}[\/.-]\d{2,4})/i,
     /Invoice Date\s*:\s*(\d{2}\/\d{2}\/\d{4})/i,
