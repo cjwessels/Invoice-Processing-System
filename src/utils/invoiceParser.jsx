744cc8cf69b05689e43ff41a0efcb7fe0f5e6046
Christiaan Wessels <97080537+cjwessels@users.noreply.github.com>
2025-05-22 10:39:58 +0200

    Updated invoiceParser.jsx

--- src/utils/invoiceParser.jsx
+++ src/utils/invoiceParser.jsx
@@ -113,7 +113,7 @@ if (text.toLowerCase().includes('matzikama')) {
   'Bridgton': 'WISPER',
 };
 
-  console.log(text.toLowerCase().includes('WISPERNET'))
+  console.log(text.toLowerCase().includes('WISPERNET') ? true : false)
 
   // If text contains "Wispernet", check for specific regions
 if (text.toLowerCase().includes('WISPERNET')) {
