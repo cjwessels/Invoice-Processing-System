23a0fe8fa95414e51a8c468bc6465953c87b444c
Christiaan Wessels <97080537+cjwessels@users.noreply.github.com>
2025-05-22 14:02:56 +0200

    Updated invoiceParser.jsx

--- src/utils/invoiceParser.jsx
+++ src/utils/invoiceParser.jsx
@@ -61,7 +61,7 @@ const extractSupplierName = (text, fileName) => {
   }
 
   console.log(text)
-  console.log((text.toLowerCase().includes('ICDLof South Africa') ? 'ICDL' : 'NADA')
+  console.log(text.toLowerCase().includes('ICDLof South Africa') ? 'ICDL' : 'NADA')
   
   if (text.toLowerCase().includes('ICDLof South Africa')) {
     const matchedSupplier = supplierCodes.find(s => s.code === 'ICDLSA');
