be1ff195dd18bdb3a72100820ed43937f1797bde
Christiaan Wessels <97080537+cjwessels@users.noreply.github.com>
2025-05-23 15:22:49 +0200

    Updated invoiceParser.jsx

--- src/utils/invoiceParser.jsx
+++ src/utils/invoiceParser.jsx
@@ -146,7 +146,7 @@ export const extractInvoiceNumber = (text, supplierName) => {
     'Trust Patrol': /Tax\s+Invoice\s(\d{1,2}\/\d{1,2}\/\d{2,4})\s+([A-Z0-9-]+)/i,
     'Matzikama Municipality - Vanrhynsdorp': /BELASTING FAKTUUR NR\.\s*(\S+)/i,
     'Matzikama Municipality - Vredendal': /BELASTING FAKTUUR NR\.\s*(\S+)/i,
-    'Matzikama Municipality - RIETPOORT': /BELASTING FAKTUUR NR\.\s*(\S+)/i,
+    'Matzikama Municipality - RIETPOORT': /TAX INVOICE NO\.\s*(\S+)/i,
     'ICDL OF SOUTH AFRICA':  /Tax\s+Invoice\s(\d{1,2}\/\d{1,2}\/\d{2,4})\s+([A-Z0-9-]+)/i,
     'Fidelity ADT': /Invoice Number:?\s+([A-Za-z0-9-]+)/i,
     'The Computer Shop' : /Invoice#\s*([A-Z0-9]+)/i,
