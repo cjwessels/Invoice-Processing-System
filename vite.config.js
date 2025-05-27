c189a24af8e4d698dd97719c5128d27d493578a2
Christiaan Wessels <97080537+cjwessels@users.noreply.github.com>
2025-05-27 10:28:30 +0200

    Fix API endpoints configuration

--- vite.config.js
+++ vite.config.js
@@ -147,6 +147,13 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   },
   optimizeDeps: {
     include: [
